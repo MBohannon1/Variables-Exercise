5a836a54efe580fb7825cf67e9c3ea1cfb61ba46
Michael Bohannon <michaelbohannon123@gmail.com>
2022-11-06 14:14:19 -0500

    Finished Variable Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Agamemnon";
+            char firstInitial = 'A';
+            int dogAge = 3;
+            bool isHungry = true;
+            double dogWeight = 182.7;
+            decimal dogHeight = 36.4m;
+
+            Console.WriteLine($"My dog's name is {dogName}. His first initial is {firstInitial}. " +
+                              $"He is {dogAge} years old and weighs {dogWeight} pounds. " +
+                              $"He is {dogHeight} inches tall.");
+            
+            Console.WriteLine($"It is {isHungry} that he is always hungry.");
+
+
         }
     }
 }
